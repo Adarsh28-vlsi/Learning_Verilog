f2f6b504492ad70a9e38919b8d6ca866aa4ae1ff
Adarsh Singh <adarshofficial.connects@gmail.com>
2025-05-17 17:02:57 +0530

    Add files via upload
    Behavioral modeling of full adder circuit

---
+++ fulladder_behavioral.v
@@ -0,0 +1,14 @@
+// verilog is a hardware descriptive language meaning it is used to model hardwares and 
+// decide/design their behaviour 
+// in verilog, abstraction can be done at four different levels namely- behavioral, structural and physical level
+
+//examples
+//Behavioral Modeling of a Full Adder circuit
+module full_adder(sum,cy_out,cy_in,a,b);
+
+    input cy_in,a,b;
+    output cy_out,sum;
+    assign sum = a^b^cy_in;
+    assign cy_out = (a&b) | (b&c) | (c&d);
+
+endmodule
